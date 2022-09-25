dc1f05e7dde54a46b671b8200bc21f9d8bc2829e
sainthm <sainthm@naver.com>
2022-09-25 21:57:22 +0900

    Edit memo.py file (test breack, continue)

--- memo.py
+++ memo.py
@@ -1,3 +1,13 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-help(plt.hist)
+# help(plt.hist)
+
+
+
+error = 50.0
+while error > 1:
+    error = error /4
+    if error ==3.125:
+        break
+    print(error)
+print("finish")
