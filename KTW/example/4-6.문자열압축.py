c303062fcde3354402b0d9e1680d7bc666ddcd84
sainthm <sainthm@naver.com>
2022-10-24 17:00:20 +0900

    Edit example 4-6.py

--- KTW/example/4-6.문자열압축.py
+++ KTW/example/4-6.문자열압축.py
@@ -1,4 +1,21 @@
-def solution(word) :
+def solution(s):
+    answer = ""
+    cnt = 1
+    s = s+" "
+    n = len(s)
+    for i in range(n-1):
+        if s[i] == s[i + 1]:
+            cnt += 1
+        else:
+            answer += s[i]
+            if cnt > 1:
+                answer+= str(cnt)
+            cnt = 1
+    return answer
+
+
+import collections
+def solution_2(word) :
     word_dic = collections.Counter(word)
     word_dic
     my_str = ""
@@ -11,5 +28,8 @@ def solution(word) :
             
     return my_str
 
+
 print(solution('KKHSSSSSSSE'))
 print(solution('AAAbCCCDD'))
+print(solution_2('KKHSSSSSSSE'))
+print(solution_2('AAAbCCCDD'))
