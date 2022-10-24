362b811831115807220c059658862e3f756277cf
sainthm <sainthm@naver.com>
2022-10-24 14:12:27 +0900

    Add example 3-2.py

---
+++ KTW/example/3-2.올바른괄호.py
@@ -0,0 +1,16 @@
+# "(())()" > O
+# "(()(()" > X
+# "())(" -> X
+
+# temp
+
+
+def solution(s):
+    answer = "YES"
+    stack = []
+    for x in s:
+        # DN_DELETE
+    return answer
+
+print(solution("(())()"))
+print(solution("())("))
