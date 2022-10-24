c42d26823f092c3c1ab908ea8933df19c7059f5e
sainthm <sainthm@naver.com>
2022-10-24 17:49:20 +0900

    Add example 4-8.py

---
+++ KTW/example/4-8.점수계산.py
@@ -0,0 +1,15 @@
+from multiprocessing.connection import answer_challenge
+
+
+def solution(nums):
+    answer = 0
+    score = 0
+    for x in nums:
+        if x == 1:
+            score += 1
+            answer += score
+        else:
+            score = 0
+    return answer
+
+print(solution([1, 0, 1, 1, 1, 0, 0, 1, 1, 0]))
