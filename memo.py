f4a8ae99332f4d19e4109b14dc886780171265d0
sainthm <sainthm@naver.com>
2022-10-27 01:51:55 +0900

    Edit memo.py

--- memo.py
+++ memo.py
@@ -1,14 +1,1 @@
-CH01_01. 머신러닝의 정의
-CH01_02. Data Split
-CH01_03. training
-CH01_04. inference
-CH01_05. feature engineering
-CH01_06. loss function
-CH01_07. evaluation metric
-CH02_01. 머신러닝 프로젝트 수행방법
-CH02_02. ML Framework Scikit-Learn
-CH03_01. 분류의 정의
-CH03_02. Model1. Linear Classifier
-CH03_03. Model2. Logistic Regression
-CH03_04. Model3. Decision Tree
-CH03_05. Model4. Random Forest
+"Applications in which the training data comprises examples of the input vectors(X) along with their corresponding target vectors(y) are known as supervised learning problems. Cases in which the aim is to assign each input  vector to one of a finite number of discrete categories, are called classification problems."
