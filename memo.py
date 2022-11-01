cd7f685d157d60b6f29c3f40825fa3df270a11a6
sainthm <sainthm@naver.com>
2022-11-01 15:02:43 +0900

    Edit memo.py

--- memo.py
+++ memo.py
@@ -1,9 +1,7 @@
-For lot
 
-So sad... 1029 - 1030.. RIP..
+X = test.drop(columns="Transported") # 학습에 사용할 feature vector
+y = test.Transported # 타겟 밸류!
 
-
-
-
-
-print(12/35)
+# 트테트테로 외워보도록 하자!
+X_test, X_val, y_test, y_val = test_test_split(X, y, test_size=0.2, random_state=42) # reproducability 를 위해 무조건 rando_state 사용
+print(X_test.shape, X_val.shape, y_test.shape, y_val.shape)
