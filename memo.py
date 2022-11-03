7b6117beb8a3f25c2804ede0934b5209be96fee4
sainthm <sainthm@naver.com>
2022-11-03 09:56:45 +0900

    Edit memo.py

--- memo.py
+++ memo.py
@@ -5,3 +5,7 @@ y = test.Transported # 타겟 밸류!
 # 트테트테로 외워보도록 하자!
 X_test, X_val, y_test, y_val = test_test_split(X, y, test_size=0.2, random_state=42) # reproducability 를 위해 무조건 rando_state 사용
 print(X_test.shape, X_val.shape, y_test.shape, y_val.shape)
+
+
+
+AI_competition_for_predicting_power_usage
