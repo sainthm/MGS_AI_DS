7955d273e48a878afe2e0e6aa640dfe40cd0dc0f
sainthm <sainthm@naver.com>
2022-11-07 01:50:13 +0900

    Edit memo.py file

--- memo.py
+++ memo.py
@@ -1,26 +1,9 @@
-# Bloeck-external-ip
+# python version update
 
-{
-    "Version": "2012-10-17",
-    "Statement": {
-        "Effect": "Deny",
-        "Action": "*",
-        "Resource": "*",
-        "Condition": {
-            "IpAddress": {
-                "Notaws:SourceIp": [
-                    "10.xxx.xxx.xxx/16",
-                    "10.xxx.xx.xx/16",
-                    "52.xxx.xxx.xxx/xx",
-                    "13.xxx.xxx.xxx/xx"
-                ]
-            },
-            "StringEqualsIfExists": {
-                "Notaws:SourceVpc": [
-                    "vpc-xxxxxx",
-                    "vpc-xxxxxx"
-                ]
-            }
-        }
-    }
-}
+# $ brew update
+
+# $ brew install python3
+
+# $ brew upgrade python3
+
+$ conda install python=3.11.0
