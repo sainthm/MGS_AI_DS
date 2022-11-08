60f1b9be3b7588602a98d8cbd99dd0a1ce41a964
sainthm <sainthm@naver.com>
2022-11-08 12:26:01 +0900

    Edit memo.py file

--- memo.py
+++ memo.py
@@ -1,9 +1,9 @@
-# python version update
-
-# $ brew update
-
-# $ brew install python3
-
-# $ brew upgrade python3
+# Upgrade python version using conda (miniforge)
 
 $ conda install python=3.11.0
+
+## Upgrade python version using python
+## python version update
+## $ brew update
+## $ brew install python3
+## $ brew upgrade python3
