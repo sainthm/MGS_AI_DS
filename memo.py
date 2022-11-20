76e1dcc33aced6eaf3a9ebb62cc95446d0c1b3d6
sainthm <sainthm@naver.com>
2022-11-21 00:33:54 +0900

    Edit memo.py file

--- memo.py
+++ memo.py
@@ -1,3 +1,18 @@
-%%time
 
-print("time")
+a = ("a", "b", "c")
+b = ("가", "나", "다")
+
+
+a1 = zip(a, b)
+
+# print(a1)
+# print(*a1)
+
+result1, result2 = zip(*a1)
+
+
+# print(result1)
+# print(result2)
+
+print(result1 == a)
+print(result2 == b)
