664be9f6cf5cbb4dc36c22f12c977df1eba6cfaa
sainthm <sainthm@naver.com>
2022-11-20 12:39:38 +0900

    Edit memo.py file

--- memo.py
+++ memo.py
@@ -1,5 +1,3 @@
-sample['광진구'] = predict(train, '광진구')
-sample['동대문구'] = predict(train, '동대문구')
-sample['성동구'] = predict(train, '성동구')
-sample['중랑구'] = predict(train, '중랑구')
-sample
+%%time
+
+print("time")
